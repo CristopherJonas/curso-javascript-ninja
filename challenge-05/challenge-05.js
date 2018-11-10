02fedcc2d5e7d51b3168790903a9d3999f7ceb59
Cristopher Jonas de Faria <CristopherJonas@users.noreply.github.com>
2018-11-10 21:31:33 -0200

    Update challenge-05.js

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -2,17 +2,21 @@
 Crie uma variável qualquer, que receba um array com alguns valores aleatórios
 - ao menos 5 - (fica por sua conta os valores do array).
 */
-// ?
+// 
+var myVar = ['1',1,null,false,'teste'];
 
 /*
 Crie uma função que receba um array como parâmetro, e retorne esse array.
 */
-// ?
+// 
+function myFunc(arr){
+return arr;
+}
 
 /*
 Imprima o segundo índice do array retornado pela função criada acima.
 */
-// ?
+console.log(myFunc(myVar)[1])
 
 /*
 Crie uma função que receba dois parâmetros: o primeiro, um array de valores; e o
@@ -20,18 +24,28 @@ segundo, um número. A função deve retornar o valor de um índice do array que
 no primeiro parâmetro. O índice usado para retornar o valor, deve ser o número passado no
 segundo parâmetro.
 */
-// ?
+// 
+
+function myFunction2(arr,index){
+return arr[index];
+}
 
 /*
 Declare uma variável que recebe um array com 5 valores, de tipos diferentes.
 */
-// ?
+// 
+var mixTypes = ['1',1,[1,2,'2'],function(){},false]; 
 
 /*
 Invoque a função criada acima, fazendo-a retornar todos os valores do último
 array criado.
 */
 // ?
+console.log(myFunction2(mixTypes,0));
+console.log(myFunction2(mixTypes,1));
+console.log(myFunction2(mixTypes,2));
+console.log(myFunction2(mixTypes,3));
+console.log(myFunction2(mixTypes,4));
 
 /*
 Crie uma função chamada `book`, que recebe um parâmetro, que será o nome do
@@ -47,12 +61,32 @@ propriedades:
 - Se o parâmetro não for passado, a função deve retornar o objeto com todos
 os livros.
 */
-// ?
+//
+function book(nomeLivro){
+  var variavel = {
+     'Livro 01':{
+         quantidadePaginas:100,
+         autor:'josé da Silva',
+         editora:'Nova Mundo'
+      },
+      'Livro 02':{
+          quantidadePaginas:90,
+          autor:'maria da Silva',
+          editora:'Tempos Modernos'
+       },
+       'Livro 03':{
+          quantidadePaginas:80,
+          autor:'Cristopher',
+          editora:'Cabeça Feita'
+        },
+    }
+  return !nomeLivro ? variavel : variavel[nomeLivro];
+}
 
 /*
 Usando a função criada acima, imprima o objeto com todos os livros.
 */
-// ?
+book();
 
 /*
 Ainda com a função acima, imprima a quantidade de páginas de um livro qualquer,
